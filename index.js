986a89c22501676974e396db5941f7ce61970c73
Jahid Hasan <jh3495689@gmail.com>
2023-04-27 11:07:55 +0600

    added server side code

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const cors = require('cors')
 const port = process.env.PORT || 5000
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
-app.use(cors()) 
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('dragon in running')
@@ -14,6 +15,26 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
     res.send(categories)
 })
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    const selectNews = news.find(p => p._id === id)
+    res.send(selectNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    if (id === 0) {
+        res.send(news)
+    }
+    const selectCategories = news.filter(n => parseInt(n.category_id) === id)
+    res.send(selectCategories)
+})
+
 
 app.listen(port, () => {
     console.log(`dragon API is running on port ${port}`)
